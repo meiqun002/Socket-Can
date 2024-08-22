b9885af2d4be5811ebbd1a4ab156448fe132dc60
meiqun002 <Meiqun.Zhang@slenergy.com>
2024-08-22 13:40:35 +0800

    增加测试类

--- src/main/java/com/slenergy/can/CanSocket.java
+++ src/main/java/com/slenergy/can/CanSocket.java
@@ -19,7 +19,7 @@ import java.util.Set;
 
 public final class CanSocket implements Closeable {
     static {
-        final String LIB_JNI_SOCKETCAN = "jni_socketcan";
+        final String LIB_JNI_SOCKETCAN = "libCanSocket";
         try {
             System.loadLibrary(LIB_JNI_SOCKETCAN);
         } catch (final UnsatisfiedLinkError e) {
