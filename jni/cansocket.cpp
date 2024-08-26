66468be0a75352972bb2b40e97623455c7139c35
meiqun002 <Meiqun.Zhang@slenergy.com>
2024-08-26 14:18:50 +0800

    修改cpp代码

--- jni/cansocket.cpp
+++ jni/cansocket.cpp
@@ -199,17 +199,10 @@ JNIEXPORT jobject JNICALL Java_com_slenergy_can_CanSocket__1recvFrame
 	memset(&frame, 0, sizeof(frame));
 	nbytes = recvfrom(fd, &frame, sizeof(frame), flags,
 			  reinterpret_cast<struct sockaddr *>(&addr), &len);
-	// if (len != sizeof(addr)) {
-	// 	throwIllegalArgumentException(env, "illegal AF_CAN address");
-	// 	return NULL;
-	// }
-	if (len != sizeof(addr)) {
-        char error_message[100];
-        sprintf(error_message, "Illegal AF_CAN address: Received len: %d, expected len: %d", len, sizeof(addr));
-        (*env)->ThrowNew(env, (*env)->FindClass(env, "java/lang/IllegalArgumentException"), error_message);
-        return NULL;
-    }
-
+	 if (len != sizeof(addr)) {
+	 	throwIllegalArgumentException(env, "illegal AF_CAN address");
+	 	return NULL;
+	 }
 	if (nbytes == -1) {
 		throwIOExceptionErrno(env, errno);
 		return NULL;
